518086a3973793a39284d43e646e605b79eab69e
Diego Rosales Rebollar <diegorila-31@hotmail.com>
2025-07-19 17:04:13 -0600

    Inicializando el proyecto

---
+++ src/test/java/com/dieborim/Biblioteca/_digital/BibliotecaDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dieborim.Biblioteca._digital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
